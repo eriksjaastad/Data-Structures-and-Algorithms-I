cdb5ad093df3654415fb95c36a112683b8ebc5e5
Erik Sjaastad <spudlogic@gmail.com>
2015-07-13 21:43:59 -0700

    This is Sparta

--- test/test.js
+++ test/test.js
@@ -1,64 +1,55 @@
-var expect = require("chai").expect;
-var app = require("../app");
-var arr = ['me', 'you', 'cup', 'can', 'phone', 'list', 'smoke', 'coffee'];
 
-describe("ArrayMethods", function() {
-  describe("Push", function() {
-    it("should add new items to the end of an array", function() {
-      arr.newPush('plant');
-      expect(arr[arr.length -1]).to.equal('plant');
+var app = require("../lib/app.js"),
+    expect = require("chai").expect;
+
+    describe("newPush", function() {
+      it("should add new items to the end of an array and array length should increase", function() {
+        var arr = [1,2,3];
+        arr.newPush(4);
+        expect(arr).to.eql([1,2,3,4]);
+        expect(arr.length).to.eql(4);
+      });
     });
-    it("should return the new length of an array", function() {
-      arr.newPush('tree');
-      expect(arr.length).to.equal(10);
+
+    describe("newPop", function() {
+      it("should return the item that was removed and should remove the last item of an array", function() {
+        var arr = [1,2,3];
+        expect(arr.newPop()).to.eql(3);
+        expect(arr.length).to.eql(2);
+      });
     });
-  });
 
-  describe("Pop", function() {
-    it("should return the item that was removed", function() {
-      expect(arr.newPop()).to.equal("tree");
+    describe("newShift", function() {
+      it("should remove the first item in an array and return it and the array length should decrease", function() {
+        var arr = [1,2,3];
+        expect(arr.newShift()).to.eql(1);
+        expect(arr).to.eql([2,3]);
+        expect(arr.length).to.eql(2);
+      });
     });
-    it("should remove the last item of an array", function() {
-      arr.newPop();
-      expect(arr.length).to.equal(10);
+
+    describe("newUnshift", function() {
+      it("should add new items to the beginning of an array and return the new length of the array", function() {
+        var arr = [1,2,3];
+        arr.newUnshift(0);
+        expect(arr).to.eql([0,1,2,3]);
+        expect(arr.length).to.eql(4);
+      });
     });
-  });
 
-  describe("Shift", function() {
-    it("should remove the first item in an array", function() {
 
+
+    describe("Unique", function() {
+      it("should take an array and return a copy of the array with all duplicates removed", function() {
+        var arr = ['alpha', 'beta', 'gamma', 'alpha'];
+        expect(app.unique(arr)).to.eql(['alpha', 'beta', 'gamma']);
+      });
     });
-    it("should return the item the item that was removed", function() {
 
+    describe("Frequency2", function() {
+      it("should take an array of words and return the most common letter in the array", function() {
+        var arr = ['mu', 'nu'];
+        expect(app.frequency2(arr)).to.eql({m:1,u:2,n:1});
+      });
     });
-    it("should change the length of an array", function() {
 
-    });
-  });
-
-  describe("Unshift", function() {
-    it("should add new items to the beginning of an array", function() {
-
-    });
-    it("should return the new length of an array", function() {
-
-    });
-  });
-
-  describe("Unique", function() {
-    it("should take an array", function() {
-
-    });
-    it("should return a copy of the array with all duplicates removed", function() {
-
-    });
-  });
-  describe("Frequency", function() {
-    it("should take an array of Engish words", function() {
-
-    });
-    it("should return the most common letter in the array", function() {
-
-    });
-  })
-});
